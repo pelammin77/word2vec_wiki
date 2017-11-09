94986dbe9ae0e1c9c983a6709ebda1d2fc74b051
Petri Lamminaho <pelammin77@gmail.com>
2017-11-09 23:14:35 +0200

    no errors but maybe something is wrong

--- read_and_proses_data.py
+++ read_and_proses_data.py
@@ -1,56 +1,42 @@
-from nltk.tokenize import sent_tokenize
-from nltk.tokenize import word_tokenize
-import gensim.models.word2vec as w2v
-from nltk.corpus import stopwords
-import multiprocessing
+import logging
 import os
-import re
-raw_data = open('wiki.test.raw', encoding="utf8").read()
-raw_data = raw_data.lower()
+from nltk import tokenize
+import multiprocessing
+from nltk.corpus import stopwords
 
-
-raw_sents = sent_tokenize(raw_data)
-
-
-
-def make_sentences_to_word_list(raw):
-
-
-    clean_text = " ".join(re.findall(r"[a-zA-Z]+", raw))
-   # clean_text =  remove_stops(clean_text)
-
-    return clean_text
 
 def remove_stops(data):
     stop_words = set(stopwords.words("english"))
-    text_without_stops = []
+    list_without_stops = []
     for w in data:
         if w not in stop_words:
-            text_without_stops.append(w)
+            list_without_stops.append(w)
+    return list_without_stops
 
-    return text_without_stops
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+raw_text = open('wiki.test.raw', encoding="utf8").read()
 
 
-tok = set(word_tokenize(raw_data))
-sentences = []
-for raw_sentence in raw_sents:
-    if len(raw_sentence) > 0:
-        sentences.append(make_sentences_to_word_list(raw_sentence))
 
-print(sentences)
-words = remove_stops(tok)
-#print( "word count:", len(words))
-#print(len(raw_sents))
-clean_text = ' '.join(words)
-clean_text = sent_tokenize(clean_text)
-#print(clean_text)
+words = [ ]
+tok = tokenize.word_tokenize(raw_text)
+tok = remove_stops(tok)
+words.append(tok)
+print(words)
+#print(toks[:50])
+
+
+from gensim.models import word2vec as w2v
+#model = word2vec.Word2Vec(words, iter=10, min_count=1, size=300, workers=4)
+
 num_features = 300
 # Minimum word count threshold.
 min_word_count = 3
 
 # Number of threads to run in parallel.
 #more workers, faster we train
-num_workers = multiprocessing.cpu_count()
+num_workers =  multiprocessing.cpu_count()
 
 # Context window length.
 context_size = 7
@@ -74,7 +60,7 @@ text2vec = w2v.Word2Vec(
     sample=downsampling
 )
 
-text2vec.build_vocab(sentences)
+text2vec.build_vocab(words)
 #print(len(words))
 
 print("Corpus size", text2vec.corpus_count)
@@ -84,6 +70,11 @@ print("Model iter size", text2vec.iter)
 
 text2vec.train(words, total_examples=text2vec.corpus_count, epochs=text2vec.iter)
 
+
+
+
+# print(model['The quick brown fox jumped over the lazy dog'])
+print(text2vec.most_similar('actor'))
 ##Saving the model
 if not os.path.exists("trained"):
     os.makedirs("trained")
--- use_word2_vec_model.py
+++ use_word2_vec_model.py
@@ -5,3 +5,5 @@ text2vec = w2v.Word2Vec.load(os.path.join("trained", "text2vec.w2v"))
 print("Corpus size", text2vec.corpus_count)
 print("Model iter size", text2vec.iter)
 
+#print(text2vec.vocabulary)
+print( text2vec.most_similar("Graph minors A survey"))
