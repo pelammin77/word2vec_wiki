1c80c446bb4395685ef43b3113d592bd0efc4310
Petri Lamminaho <pelammin77@gmail.com>
2017-11-14 02:44:58 +0200

    change the model folder name

--- use_word2_vec_model.py
+++ use_word2_vec_model.py
@@ -1,9 +1,9 @@
 import gensim.models.word2vec as w2v
 import os
 
-text2vec = w2v.Word2Vec.load(os.path.join("trained", "text2vec.w2v"))
+text2vec = w2v.Word2Vec.load(os.path.join("trained_model", "corpus2vec.w2v"))
 print("Corpus size", text2vec.corpus_count)
 print("Model iter size", text2vec.iter)
 
 #print(text2vec.vocabulary)
-print( text2vec.most_similar("Graph minors A survey"))
+print( text2vec.most_similar("president"))
